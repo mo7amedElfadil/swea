40b90367079715cf1a2f0e5f0a4926aab54e448c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2025-01-30 18:24:01 +0300

    Incorporate htmx and add response decorator

--- api/v1/views/main.py
+++ api/v1/views/main.py
@@ -1,5 +1,11 @@
+from flask import make_response, request
 from api.v1.views import bp
+from utils.view_modifiers import response
 
 @bp.route("/", methods=["GET", "POST"])
+@response(template_file="index.html")
 def index():
-    return "Hello, World!"
+    if request.headers.get("hx-request"):
+        html = '<h2> this markup from backend, open network tab</h2>'
+        return make_response(html)
+    return {"user" : "John Doe"}

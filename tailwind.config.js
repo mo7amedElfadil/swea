e1a9668e024ebce5556ede0b432454ca7bf919e3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2025-02-22 04:35:08 +0300

    Update fluid grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       gridTemplateColumns: {
-        fluid: "repeat(auto-fit, minmax(350px, 400px))",
+        fluid: "repeat(auto-fit, minmax(300px, 450px))",
       },
       backgroundSize: {
         900: "900px", // Add your custom size here

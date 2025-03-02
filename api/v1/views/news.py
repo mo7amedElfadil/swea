db1d73106d591b6928e80e4cb304618c686a2662
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2025-03-02 03:48:58 +0300

    Add boiler-plate for news view

--- api/v1/views/news.py
+++ api/v1/views/news.py
@@ -10,26 +10,29 @@ from utils.view_modifiers import response
 @bp.route("/dashboard/news/form", methods=["GET"])
 @response(template_file="partials/dashboard/news-form.html")
 def retrieve_form():
-    """get news form"""
+    """Get news form"""
     return dict()
 
 @bp.route("/dashboard/news/create", methods=["POST"])
-@response(template_file="partials/dashboard/news-form.html")
 def create_news():
-    """Add a new news"""
+    """Create news"""
     news_form = request.form.to_dict()
     img = request.files.get('image')
     news_form['image'] = img
-    news_data = NewsService().create(news_form)
-    return dict()
-
-@bp.route("/dashboard/news/update", methods=["UPDATE"])
-@response(template_file="partials/dashboard/news-form.html")
-def update_news():
-    """Add a new news"""
-    if request.headers.get('hx-request') and request.method == "UPDATE":
-      news_form = request.form.to_dict()
-      img = request.files.get('image')
-      news_form['image'] = img
+    try:
       news_data = NewsService().create(news_form)
-    return dict()
+    except Exception:
+      pass
+    return make_response()
+
+@bp.route("/dashboard/news/update", methods=["PATCH"])
+def update_news():
+    """Update news"""
+    news_form = request.form.to_dict()
+    return make_response()
+
+
+@bp.route("/dashboard/news/delete", methods=["DELETE"])
+def delete_news():
+    """Delelte news"""
+    return make_response()

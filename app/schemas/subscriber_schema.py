8bbbd9b62a0c9df40a27f3625eaf7d342ff04743
Mohabdo21 <mohannadabdo21@hotmail.com>
2025-05-15 03:58:41 +0400

    feat: improves email validation using marshmallow's built-in validator.

--- app/schemas/subscriber_schema.py
+++ app/schemas/subscriber_schema.py
@@ -1,6 +1,4 @@
-import re
-
-from marshmallow import Schema, ValidationError, fields, validates
+from marshmallow import Schema, fields, validate
 
 
 class SubscriberSchema(Schema):
@@ -11,14 +9,5 @@ class SubscriberSchema(Schema):
     email = fields.Str(
         required=True,
         error_messages={"required": "Subscriber Email is required."},
+        validate=validate.Email(),
     )
-
-    @validates("email")
-    def validate_email(self, value: str):
-        """Validate that the email is not empty."""
-        if not value.strip():
-            raise ValidationError("Email cannot be empty.")
-
-        email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        if not isinstance(value, str) or not re.match(email_pattern, value):
-            raise ValidationError("Invalid email format in 'email' field.")
